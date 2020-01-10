22e3b422afadf630d53e049d482524959e068c4a
NaveedG <nganjani@protonmail.com>
2020-01-10 11:28:01 -0600

    added time of day column

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'tod'
+# test = Tod::TimeOfDay.new(8)
+# p test.to_formatted_s
+
 
 Administrator.create(username: 'navganj', password: 'password', email_address: 'navganj@gmail.com')
 
-Event.create(first_name: 'Zayd', last_name: 'Awwal', date: Date.new(2005, 05, 06), administrator_id: 1)
-Event.create(first_name: 'Amr', last_name: 'Thani', date: Date.new(2020, 01, 15), administrator_id: 1)
+Event.create(first_name: 'Zayd', last_name: 'First', date: Date.new(2005, 05, 06), time: Tod::TimeOfDay.new(20).to_formatted_s, administrator_id: 1)
+Event.create(first_name: 'Amr', last_name: 'Second', date: Date.new(2020, 01, 15), time: Tod::TimeOfDay.new(12).to_formatted_s, administrator_id: 1)
+Event.create(first_name: 'Hind', last_name: 'Third', date: Date.new(2020, 01, 15), time: Tod::TimeOfDay.new(15).to_formatted_s, administrator_id: 1)
